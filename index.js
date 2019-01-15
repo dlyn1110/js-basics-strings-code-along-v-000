737b2257f686e13d48abf4485a7965db1e32efdb
Dani Lyn <dlyn1110@gmail.com>
2019-01-15 03:15:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
-const shortGreeting = ... ;
+
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ';
